d33c12850702161f166f98bc0c029dbb26c6f7fe
Gerbert Pesquera <gerbert.pesquera@gmail.com>
2022-05-10 11:02:45 -0400

    commit tree ea55ac1443766d0fab7dd317fa8cfcbf1221e66f

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+
+document.addEventListener('DOMContentLoaded', function() {
+    let text = document.getElementById("text")
+    console.log(text)
+    text.textContent = "This is really cool!"
+  });
